403017ade80fbeba98edf6f6ba1ac0993fb6d783
phatcharapornbu-lab <phatcharaporn.bu@kkumail.com>
2025-12-02 10:53:47 +0700

    Initial commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+
+        {
+            // Print "Hello, World!" to the console
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("my name is phatcharaporn Buuphasing");
+            Console.WriteLine("student id 683450059-3");
+            Console.WriteLine("major: computer and Information science");
+            Console.WriteLine("faculty: Faculty ");
+            Console.WriteLine("khon Kaen University");
+        }
+    }
+}
